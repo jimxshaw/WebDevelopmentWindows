3b619fe5c123f36d8f7c926c94a4207cfd0c5bba
James <jimxshaw@gmail.com>
2016-03-03 10:47:50 -0600

    refactored book routing by adding middleware to reduce code repeat. refactored GET. implemented PUT and PATCH http verbs.

--- REST-sample/Routes/bookRoutes.js
+++ REST-sample/Routes/bookRoutes.js
@@ -46,38 +46,76 @@ var routes = function (Book) {
             });
         });
 
+    // DRY means Don't Repeat Yourself. Our book routes will involve repeated code so that means it's time to add some
+    // middleware to handle that. What this middleware does is it's going to do a findById and to find the book id. If
+    // there's an error, an error will return. If the book exists, add it to the request.
+    bookRouter.use("/:bookId", function (req, res, next) {
+        // The .findById method is used instead of .find and then we're passing in a book id as the first argument.
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else if (book) {
+                // This will make req.book available everywhere down stream to be used.
+                req.book = book;
+                // Next() simply means moving on to the next middleware along the chain.
+                next();
+            }
+            else {
+                // If the book isn't found, return status 404.
+                res.status(404).send("No book found");
+            }
+        });
+    });
+
     // Routing to GET, PUT, PATCH a particular book by its id.
     bookRouter.route("/:bookId")
         .get(function (req, res) {
-            // The .findById method is used instead of .find and then we're passing in a book id as the first argument.
-            Book.findById(req.params.bookId, function (err, book) {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                else {
-                    res.json(book);
-                }
-            });
+            // The user's request would hit our middleware with our Book.findById() before ever reaching here. If the
+            // book exists then the middleware assigns it to req.book, the same req.book that's passed into res.json below.
+            // If the book doesn't exist then the req will never reach here because a 404 status would have displayed.
+            res.json(req.book);
         })
-        .put(function(req, res) {
+        .put(function (req, res) {
             // PUT aka update is similar to GET except that the existing book's property values are updated with the
             // values that come in from the request. If the request values are empty then that particular book's updated
             // values will be empty as well.
-            Book.findById(req.params.bookId, function (err, book) {
+            // Essentially whatever exists in req.book will be updated with what's in req.body.
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+
+            // Saves the update to mongodb while handling errors.
+            req.book.save(function (err) {
                 if (err) {
                     res.status(500).send(err);
                 }
                 else {
-                    book.title = req.body.title;
-                    book.author = req.body.author;
-                    book.genre = req.body.genre;
-                    book.read = req.body.read;
-                    book.save(); // Saves the update to mongodb.
-                    res.json(book);
+                    res.json(req.book);
                 }
             });
         })
-        .patch();
+        .patch(function (req, res) {
+            // We're assigning everything in req.body over to req.book but we do not want to update the book id. We add
+            // an if statement to remove the id from the request body so that it will never be assigned to req.book.
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            for (var property in req.body) {
+                req.book[property] = req.body[property];
+            }
+
+            req.book.save(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(req.book);
+                }
+            });
+        });
 
     return bookRouter;
 };
